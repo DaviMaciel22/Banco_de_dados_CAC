0eb94c801d88efea5149b4beae7182b7301688ba
Alan Lopes <lopesalan2007@gmail.com>
2026-05-01 19:06:09 +0000

    primeiros ajustes 1

--- estrutura.sql
+++ estrutura.sql
@@ -68,13 +68,13 @@ CREATE TABLE  Entrada(
     data_compra DATETIME NOT NULL,
     valor_compra numeric(18,2) NOT NULL,
     quantidade_compra BIGINT NOT NULL,
-    PRIMARY KEY(fkfornecedor)
+    PRIMARY KEY(fkfornecedor, fkproduto)
 );
 
 CREATE TABLE Categoria_prod_set(
     fksetor BIGINT NOT NULL,
     fkcategoria BIGINT NOT NULL,
-    PRIMARY KEY(fkcategoria)
+    PRIMARY KEY(fkcategoria, fksetor)
 );
 
 CREATE TABLE Telefone(
@@ -88,23 +88,22 @@ CREATE TABLE Telefone(
 
 
 
-ALTER TABLE Endereco ADD CONSTRAINT FK_Endereco_Fornecedor FOREIGN KEY (fkfornecedor) REFERENCES Fornecedor(id_fornecedor);
-ALTER TABLE Endereco ADD CONSTRAINT FK_Endereco_Funcionario FOREIGN KEY (fkfuncionario) REFERENCES Funcionario(id_funcionario);
-ALTER TABLE Endereco ADD CONSTRAINT FK_Endereco_Consumidor FOREIGN KEY (fkconsumidor) REFERENCES Consumidor(id_consumidor);
+ALTER TABLE Endereco_Fornecedor ADD CONSTRAINT FK_Endereco_Fornecedor FOREIGN KEY (fkfornecedor) REFERENCES Fornecedor(id_fornecedor);
+
+ALTER TABLE Endereco_Funcionario ADD CONSTRAINT FK_Endereco_Funcionario FOREIGN KEY (fkfuncionario) REFERENCES Funcionario(id_funcionario);
 
 ALTER TABLE Produto ADD CONSTRAINT FK_Produto_Categoria FOREIGN KEY (fkcategoria) REFERENCES Categoria_Produto(id_categoria);
 
 ALTER TABLE Funcionario ADD CONSTRAINT FK_Funcionario_Setor FOREIGN KEY (fksetor) REFERENCES Setor(id_setor);
 
-ALTER TABLE Vendas ADD CONSTRAINT FK_Vendas_Produto FOREIGN KEY (fkproduto) REFERENCES Produto(id_produto);
-ALTER TABLE Vendas ADD CONSTRAINT FK_Vendas_Consumidor FOREIGN KEY (fkconsumidor) REFERENCES Consumidor(id_consumidor);
+ALTER TABLE Saida ADD CONSTRAINT FK_Saida_Produto FOREIGN KEY (fkproduto) REFERENCES Produto(id_produto);
+ALTER TABLE Saida ADD CONSTRAINT FK_Saida_Setor FOREIGN KEY (fksetor) REFERENCES Setor(id_setor);
 
-ALTER TABLE Compras ADD CONSTRAINT FK_Compras_Produto FOREIGN KEY (fkproduto) REFERENCES Produto(id_produto);
-ALTER TABLE Compras ADD CONSTRAINT FK_Compras_Fornecedor FOREIGN KEY (fkfornecedor) REFERENCES Fornecedor(id_fornecedor);
+ALTER TABLE Entrada ADD CONSTRAINT FK_Compras_Produto FOREIGN KEY (fkproduto) REFERENCES Produto(id_produto);
+ALTER TABLE Entrada ADD CONSTRAINT FK_Compras_Fornecedor FOREIGN KEY (fkfornecedor) REFERENCES Fornecedor(id_fornecedor);
 
 ALTER TABLE Categoria_prod_set ADD CONSTRAINT FK_Categoria_prod_set_Setor FOREIGN KEY (fksetor) REFERENCES Setor(id_setor);
 ALTER TABLE Categoria_prod_set ADD CONSTRAINT FK_Categoria_prod_set_Categoria FOREIGN KEY (fkcategoria) REFERENCES Categoria_Produto(id_categoria);
 
 ALTER TABLE Telefone ADD CONSTRAINT FK_Telefone_Funcionario FOREIGN KEY (fkfuncionario) REFERENCES Funcionario(id_funcionario);
 ALTER TABLE Telefone ADD CONSTRAINT FK_Telefone_Fornecedor FOREIGN KEY (fkfornecedor) REFERENCES Fornecedor(id_fornecedor);
-ALTER TABLE Telefone ADD CONSTRAINT FK_Telefone_Consumidor FOREIGN KEY (fkconsumidor) REFERENCES Consumidor(id_consumidor);
